57b55c7fd0c0d2300c95beaa9863d4523240bf5b
Gerard de Brieder <smeevil@me.com>
2020-07-27 13:14:20 +0200

    fixed the autofix

--- rules/enforce-svg-import.js
+++ rules/enforce-svg-import.js
@@ -1,24 +1,19 @@
 const reportError = (context, node) => {
-  const parts = node.source.raw.split(".");
-  //  const name = parts[1].replace("/", "");
+  const parts = node.source.raw.split(".")
   const name = parts[1]
   .replace(/[A-Z\xC0-\xD6\xD8-\xDE]?[a-z\xDF-\xF6\xF8-\xFF]+|[A-Z\xC0-\xD6\xD8-\xDE]+(?![a-z\xDF-\xF6\xF8-\xFF])|\d+/g, (word) => word.charAt(0).toUpperCase() + word.slice(1))
-  .replace(/[\W+|_]/g, "");
-  console.log("name", name);
-  const fix = `import { ReactComponent as ${name} } from ${node.source.raw}`;
+  .replace(/[\W+|_]/g, "")
+  const fix = `import { ReactComponent as ${name} } from ${node.source.raw}`
   
   context.report({
     node,
     loc: node.loc,
     message: `Invalid svg import`,
-    suggest: {
-      message: `Please use "${fix}"`,
-      fix(fixer) {
-        return [fixer.replaceText(node, fix)];
-      }
+    fix(fixer) {
+      return [fixer.replaceText(node, fix)]
     }
-  });
-};
+  })
+}
 
 module.exports = {
   meta: {
@@ -33,14 +28,14 @@ module.exports = {
   create(context) {
     return {
       ImportDeclaration(node) {
-        if (node.source.type !== "Literal") return;
-        if (!node.source.raw.match(/\.svg/)) return;
+        if (node.source.type !== "Literal") return
+        if (!node.source.raw.match(/\.svg/)) return
         if (node.specifiers[0] && node.specifiers[0].imported && node.specifiers[0].imported.name === "ReactComponent") {
-          return;
+          return
         }
         
-        reportError(context, node);
+        reportError(context, node)
       }
-    };
+    }
   }
-};
+}
